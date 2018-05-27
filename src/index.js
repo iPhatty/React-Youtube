c353de8ab8fda5182b08b043dcfa5ad5fb13897a
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-27 23:21:29 +0100

    install youtube-search-api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const env = require('env2')('env')
+
+const API_KEY = process.env.API_KEY;
 
 const App = () => {
     return <div>Hi!</div>;
