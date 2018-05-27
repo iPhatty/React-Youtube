4faf23171a57e024f46e0e9df1342959db3c1ea8
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-28 00:39:52 +0100

    set class for App component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 const env = require('env2')('env')
-
+const API_KEY = process.env.API_KEY;
 // Import components
 import SearchBar from './components/search_bar'
 
-const API_KEY = process.env.API_KEY;
+YTSearch({key: API_KEY, term: 'bts fake love'}, function(data){
+    console.log(data);
+});
 
-const App = () => {
+class App extends Component {
+constructor(props){
+    super(props);
+
+    this.state = { };
+}
+
+render(){
     return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+        <div>
+            <SearchBar />
+        </div>
+        );
+}    
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
