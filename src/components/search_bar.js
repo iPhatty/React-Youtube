387a5ba278d6604aff4dd854445adbd3da366781
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-27 23:34:11 +0100

    export search_bar to index.js

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeachBar = () => {
+    return <input />;
+};
+
+export default SeachBar;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const env = require('env2')('env')
 
+// Import components
+import SearchBar from './components/search_bar'
+
 const API_KEY = process.env.API_KEY;
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
