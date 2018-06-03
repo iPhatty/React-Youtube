33464fac0ed8d6f3152a36d5b2724f7a044dcef7
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-03 18:13:51 +0100

    videosearch debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -30,9 +31,10 @@ videoSearch (searchTerm) {
 }
 
 render(){
+    const videoSearch = _.debounce((searchTerm) => {this.videoSearch(searchTerm)} , 300);
     return (
         <div>
-            <SearchBar onSearchChange={ searchTerm => this.videoSearch(searchTerm) } />
+            <SearchBar onSearchChange={ videoSearch } />
             <VideoDetail video={this.state.selectedVideo} />
             <VideoList 
             onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
